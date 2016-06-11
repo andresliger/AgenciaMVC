dbd648568d877dd0da0c5cab180218154c035b32
andresliger <andresligerg@gmail.com>
2016-06-11 08:25:22 -0500

    Actualización en el modelo persistencia, vista nueva

---
+++ AgenciaSolution/Modelo/FacadeAgencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class FacadeAgencia
+    {
+        public void insertarAgencia(AGENCIA objA)
+        {
+            using (var db = new AgenciaContext())
+            {
+                db.AGENCIAs.Add(objA);
+                db.SaveChanges();
+            }
+        }
+
+        public void modificarAgencia(AGENCIA objA)
+        {
+            using (var db = new AgenciaContext())
+            {
+                //db.AGENCIAs.
+                //db.SaveChanges();
+            }
+        }
+    }
+}
